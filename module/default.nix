c56e32da0a942c0fe136fdf33761b9158c254396
Silvan Schmidt <mail@silvanschmidt.ch>
2025-02-16 21:06:16 +0100

    refactor: format

--- module/default.nix
+++ module/default.nix
@@ -3,66 +3,68 @@
 {
   hardware.graphics.enable = true;
 
-  home-manager.users."${systemInformation.userName}" = { ... }: {
-    wayland.windowManager.sway =
-      let
-        modifier = "Mod4";
-      in
-      {
-        enable = true;
+  home-manager.users."${systemInformation.userName}" =
+    { ... }:
+    {
+      wayland.windowManager.sway =
+        let
+          modifier = "Mod4";
+        in
+        {
+          enable = true;
 
-        config = {
-          inherit modifier;
+          config = {
+            inherit modifier;
 
-          focus.followMouse = "no";
+            focus.followMouse = "no";
 
-          input."*" = {
-            tap = "enabled";
-            middle_emulation = "enabled";
-          };
+            input."*" = {
+              tap = "enabled";
+              middle_emulation = "enabled";
+            };
 
-          keybindings = {
-            "${modifier}+Return" = "exec ${config.nebunix.defaultPrograms.terminal}";
-            "${modifier}+f" = "exec ${config.nebunix.defaultPrograms.browser}";
+            keybindings = {
+              "${modifier}+Return" = "exec ${config.nebunix.defaultPrograms.terminal}";
+              "${modifier}+f" = "exec ${config.nebunix.defaultPrograms.browser}";
 
-            "${modifier}+j" = "focus down";
-            "${modifier}+k" = "focus up";
-            "${modifier}+h" = "focus left";
-            "${modifier}+l" = "focus right";
+              "${modifier}+j" = "focus down";
+              "${modifier}+k" = "focus up";
+              "${modifier}+h" = "focus left";
+              "${modifier}+l" = "focus right";
 
-            "${modifier}+Shift+j" = "move down";
-            "${modifier}+Shift+k" = "move up";
-            "${modifier}+Shift+h" = "move left";
-            "${modifier}+Shift+l" = "move right";
+              "${modifier}+Shift+j" = "move down";
+              "${modifier}+Shift+k" = "move up";
+              "${modifier}+Shift+h" = "move left";
+              "${modifier}+Shift+l" = "move right";
 
-            "${modifier}+1" = "workspace number 1";
-            "${modifier}+2" = "workspace number 2";
-            "${modifier}+3" = "workspace number 3";
-            "${modifier}+4" = "workspace number 4";
-            "${modifier}+5" = "workspace number 5";
-            "${modifier}+6" = "workspace number 6";
-            "${modifier}+7" = "workspace number 7";
-            "${modifier}+8" = "workspace number 8";
-            "${modifier}+9" = "workspace number 9";
-            "${modifier}+0" = "workspace number 10";
+              "${modifier}+1" = "workspace number 1";
+              "${modifier}+2" = "workspace number 2";
+              "${modifier}+3" = "workspace number 3";
+              "${modifier}+4" = "workspace number 4";
+              "${modifier}+5" = "workspace number 5";
+              "${modifier}+6" = "workspace number 6";
+              "${modifier}+7" = "workspace number 7";
+              "${modifier}+8" = "workspace number 8";
+              "${modifier}+9" = "workspace number 9";
+              "${modifier}+0" = "workspace number 10";
 
-            "${modifier}+Shift+1" = "move container to workspace number 1";
-            "${modifier}+Shift+2" = "move container to workspace number 2";
-            "${modifier}+Shift+3" = "move container to workspace number 3";
-            "${modifier}+Shift+4" = "move container to workspace number 4";
-            "${modifier}+Shift+5" = "move container to workspace number 5";
-            "${modifier}+Shift+6" = "move container to workspace number 6";
-            "${modifier}+Shift+7" = "move container to workspace number 7";
-            "${modifier}+Shift+8" = "move container to workspace number 8";
-            "${modifier}+Shift+9" = "move container to workspace number 9";
-            "${modifier}+Shift+0" = "move container to workspace number 10";
+              "${modifier}+Shift+1" = "move container to workspace number 1";
+              "${modifier}+Shift+2" = "move container to workspace number 2";
+              "${modifier}+Shift+3" = "move container to workspace number 3";
+              "${modifier}+Shift+4" = "move container to workspace number 4";
+              "${modifier}+Shift+5" = "move container to workspace number 5";
+              "${modifier}+Shift+6" = "move container to workspace number 6";
+              "${modifier}+Shift+7" = "move container to workspace number 7";
+              "${modifier}+Shift+8" = "move container to workspace number 8";
+              "${modifier}+Shift+9" = "move container to workspace number 9";
+              "${modifier}+Shift+0" = "move container to workspace number 10";
 
-            "${modifier}+v" = "splith";
-            "${modifier}+s" = "splitv";
+              "${modifier}+v" = "splith";
+              "${modifier}+s" = "splitv";
 
-            "${modifier}+Shift+f" = "floating toggle";
+              "${modifier}+Shift+f" = "floating toggle";
+            };
           };
         };
-      };
-  };
+    };
 }

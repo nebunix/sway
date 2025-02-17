a98af7845620398d104c381b3cf4378f408e9f7f
Silvan Schmidt <mail@silvanschmidt.ch>
2025-02-17 19:18:01 +0100

    feat: use volume keys

--- module/default.nix
+++ module/default.nix
@@ -81,6 +81,10 @@
 
               "XF86MonBrightnessDown" = "exec brightnessctl set 5%-";
               "XF86MonBrightnessUp" = "exec brightnessctl set 5%+";
+
+              "XF86AudioMute" = "exec wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle";
+              "XF86AudioLowerVolume" = "exec wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-";
+              "XF86AudioRaiseVolume" = "exec wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+";
             };
           };
         };
